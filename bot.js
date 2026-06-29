313c6bac34ca8e212b8bdbf0c7ee4a0943549785
Sokorev Vladimir <sokorev.sod@yandex.ru>
2026-06-29 15:17:10 +0800

    Add Telegram bot

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api').default;
 
-const token = '8831313711:AAHA1XuI1x6OGW4KilT2W6ywQKahsTLtqoc';
+const token = 'TOKEN HERE';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/, (msg) => {
