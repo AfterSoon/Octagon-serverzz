42ab8a4a7c6d262680171f7502ce37d773606106
Sokorev Vladimir <sokorev.sod@yandex.ru>
2026-06-30 19:31:47 +0800

    Add bot commands: help, site, creator

--- bot.js
+++ bot.js
@@ -1,10 +1,41 @@
 const TelegramBot = require('node-telegram-bot-api').default;
 
+// Вставь сюда свой токен от BotFather
 const token = 'TOKEN HERE';
-const bot = new TelegramBot(token, {polling: true});
 
+// Создаем бота
+const bot = new TelegramBot(token, { polling: true });
+
+// Команда /start
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Привет, октагон!');
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привет, октагон!');
+});
+
+// Команда /help - список команд
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const helpText = `
+📋 Список команд:
+
+/help - список команд с описанием
+/site - ссылка на сайт Октагона
+/creator - ФИО создателя бота
+    `.trim();
+    
+    bot.sendMessage(chatId, helpText);
+});
+
+// Команда /site - ссылка на сайт
+bot.onText(/\/site/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Сайт Октагона: https://octagon-students.ru/');
+});
+
+// Команда /creator - ФИО создателя
+bot.onText(/\/creator/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Создатель бота: Сокорев Владимир');
 });
 
 console.log('Бот запущен...');
